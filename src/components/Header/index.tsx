18c0b403639edb4492377ba995f74214becf86f4
tiger1028 <gentleman.tiger1028@gmail.com>
2024-08-18 18:08:55 +0300

    feat: all files changed

--- src/components/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-
-import { Header, Title } from './styles';
-export const HeaderComponent: React.FC = () => {
-    return (
-        <Header>
-            <Title>My first Project. Welcome!</Title>
-        </Header>
-    )
-}
